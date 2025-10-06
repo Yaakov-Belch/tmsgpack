ae22d88cee9e9f839c54c44a5be374f81c61d3de
Yaakov Belch <git-commit@yaakovnet.net>
2025-10-06 12:23:40 +0300

    speed optimization: wr_window => wr

--- JavaScript/src/buffers.js
+++ JavaScript/src/buffers.js
@@ -2,9 +2,11 @@ import { TMsgpackEncodingError, TMsgpackDecodingError } from "./exceptions.js"
 
 export class EncodeBuffer {
     constructor(initialSize, maxByteLength) {
-        this.buffer  = new ArrayBuffer(initialSize, {maxByteLength});
-        this.pos     = 0;
-        this.max_pos = initialSize;
+        this.buffer     = new ArrayBuffer(initialSize, {maxByteLength});
+        this.data_view  = new DataView(this.buffer);
+        this.uint8Array = new Uint8Array(this.buffer);
+        this.pos        = 0;
+        this.max_pos    = initialSize;
 
         this.textEncoder = new TextEncoder()
         this.collator = new Intl.Collator('en', {
@@ -14,7 +16,7 @@ export class EncodeBuffer {
         });
     }
 
-    wr_window(n, asUint8Array=false) {
+    wr(method, n, value, setUint8Array=false) {
         const old_pos = this.pos;
         this.pos += n;
 
@@ -23,70 +25,37 @@ export class EncodeBuffer {
             let new_size = this.max_pos;
             while (new_size < this.pos) { new_size *= 2; }
             this.buffer.resize(new_size)
-            this.max_pos = new_size;
+            this.max_pos    = new_size;
+            this.data_view  = new DataView(this.buffer);
+            this.uint8Array = new Uint8Array(this.buffer);
         }
 
-        return asUint8Array
-            ? new Uint8Array(this.buffer, old_pos, n)
-            : new DataView(this.buffer, old_pos, n);
+        if(setUint8Array) {
+          this.uint8Array[method](value, old_pos)       // method should be 'set'
+        } else {
+          this.data_view[method](old_pos, value, true)     // true means little-endian
+        }
+        return this
     }
 
-    put_bytes(value) {  // bytes means: Uint8Array
-        this.wr_window(value.length, true).set(value)
-        return this;
-    }
+    // 'bytes' means: Uint8Array
+    put_bytes(value)  { return this.wr('set', value.length, value, true) }
 
-    put_int1(value) {
-        this.wr_window(1).setInt8(0, value, true);
-        return this;
-    }
+    put_int1(value)   { return this.wr('setInt8',      1, value) }
+    put_int2(value)   { return this.wr('setInt16',     2, value) }
+    put_int4(value)   { return this.wr('setInt32',     4, value) }
+    put_int8(value)   { return this.wr('setBigInt64',  8, BigInt(value)) }
 
-    put_int2(value) {
-        this.wr_window(2).setInt16(0, value, true); // true = little-endian
-        return this;
-    }
+    put_uint1(value)  { return this.wr('setUint8',     1, value) }
+    put_uint2(value)  { return this.wr('setUint16',    2, value) }
+    put_uint4(value)  { return this.wr('setUint32',    4, value) }
+    put_uint8(value)  { return this.wr('setBigUint64', 8, BigInt(value)) }
 
-    put_int4(value) {
-        this.wr_window(4).setInt32(0, value, true);
-        return this;
-    }
-
-    put_int8(value) {
-        this.wr_window(8).setBigInt64(0, BigInt(value), true);
-        return this;
-    }
-
-    put_uint1(value) {
-        this.wr_window(1).setUint8(0, value, true);
-        return this;
-    }
-
-    put_uint2(value) {
-        this.wr_window(2).setUint16(0, value, true);
-        return this;
-    }
-
-    put_uint4(value) {
-        this.wr_window(4).setUint32(0, value, true);
-        return this;
-    }
-
-    put_uint8(value) {
-        this.wr_window(8).setBigUint64(0, BigInt(value), true);
-        return this;
-    }
-
-    put_float4(value) {
-        this.wr_window(4).setFloat32(0, value, true);
-        return this;
-    }
-
-    put_float8(value) {
-        this.wr_window(8).setFloat64(0, value, true);
-        return this;
-    }
+    put_float4(value) { return this.wr('setFloat32',   4, value) }
+    put_float8(value) { return this.wr('setFloat64',   8, value) }
 
     as_bytes() {
+        // Copy buffer slice so that the result is not resizable!
         return new Uint8Array(this.buffer.slice(0, this.pos));
     } 
 }
